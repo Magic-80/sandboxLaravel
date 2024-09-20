be6a57ac624f5681c757958df603d13acbda0c2f
Magic-80 <developper.magic@gmail.com>
2024-09-20 10:28:37 +0200

    update : kernel for minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new JobTest)->dailyAt('10:25');
+        $schedule->job(new JobTest)->everyMinute();
     }
 
     /**
