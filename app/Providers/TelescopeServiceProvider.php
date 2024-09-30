1c571f6c65efde48151bbc9295ca653153d86d36
Magic-80 <developper.magic@gmail.com>
2024-09-30 11:44:38 +0200

    update : composer.json telescopeProvider

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -55,6 +55,10 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     protected function gate(): void
     {
-       //
+        Gate::define('viewTelescope', function ($user) {
+            return in_array($user->email, [
+                'theo@gmail.com'
+            ]);
+        });
     }
 }
